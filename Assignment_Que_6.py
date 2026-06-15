6787ed964d0aed8bff2c6fc8f20186f18bea3acf
abhijitjagtap021-tech <abhijitjagtap021@gmail.com>
2026-06-15 20:17:20 +0530

    Add files via upload
    # Write a program which takes two numbers in input and prints there addition, Subtraction and Multiplication
    
    C:\Users\Hp\Desktop\Python>python Assignment_Que_6.py
    Enter your first number
    20
    Enter your second number
    10
    -----------Output - Addition, Subtraction,Multiplication ------------------
    30
    10
    200

---
+++ Assignment_Que_6.py
@@ -0,0 +1,19 @@
+# Write a program which takes two numbers in input and prints there addition, subscription and Multiplication
+
+print("Enter your first number")    # a = 20 
+a = int(input())                    # Integer number input 
+
+print("Enter your second number")   # b = 10
+b = int(input())                    # Integer number input
+
+
+print("-----------Output - Addition, Subtraction,Multiplication ------------------")
+
+Addition =  a + b
+print(Addition)                      # output - 30
+
+Subtraction =  a - b
+print(Subtraction)                   # Output - 10
+
+Multiplication =  a * b
+print(Multiplication)                # Output - 200
