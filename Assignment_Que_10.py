6c58e6e05c705d669f86ae7cad8e58094366ea4c
abhijitjagtap021-tech <abhijitjagtap021@gmail.com>
2026-06-15 20:54:53 +0530

    Add files via upload
    # Write a output of below program
    
    print("10" + "20")   # Double Quotes takes values as string and + operator concatenate number 10 and 20 - Output - 1020 
    print(10 + 20)       # Output - Addition of two number as it is integer.
    
    
    Output - C:\Users\Hp\Desktop\Python>python Assignment_Que_10.py
    1020
    30

---
+++ Assignment_Que_10.py
@@ -0,0 +1,4 @@
+# Write a output of below program
+
+print("10" + "20")   # Double Quotes takes values as string and + operator concatenate number 10 and 20 - Output - 1020 
+print(10 + 20)       # Output - Addition of two number as it is integer.
