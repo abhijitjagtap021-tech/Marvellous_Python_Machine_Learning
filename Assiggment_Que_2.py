e7ae4735706f9f8c55df95cceb095331966ce9f4
abhijitjagtap021-tech <abhijitjagtap021@gmail.com>
2026-06-15 19:47:39 +0530

    Update Assiggment_Que_2.py

--- Assiggment_Que_2.py
+++ Assiggment_Que_2.py
@@ -6,7 +6,7 @@ b = 10
 print(id(a))  # a=10 - output - Returns same Memory Address - 140726350103960
 print(id(b))  # b=10 - output - Returns same Memory Address - 140726350103960
 
-# Writa a code a= [10] and b = [10] using id function. It is list data type which stores data/value in different memory address for each list. 
+# Write a code a= [10] and b = [10] using id function. It is list data type which stores data/value in different memory address for each list. 
 
 a = [10]
 b = [10]
