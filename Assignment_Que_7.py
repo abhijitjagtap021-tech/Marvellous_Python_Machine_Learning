d25afabfa63667a1e59fba6ef47f50f258448c1e
abhijitjagtap021-tech <abhijitjagtap021@gmail.com>
2026-06-15 21:07:20 +0530

    Add files via upload
     Why does the input() function always return a string?
    How can you convert it to another data type?"
    
    Input function as well take string as input so we need to define int or float explicitly in front of the input() function.
    
    Output of this program is inter data type as we explicitly define int.

---
+++ Assignment_Que_7.py
@@ -0,0 +1,4 @@
+# Display output of below program
+
+X = int(input("Enter Number :"))
+print(type(X))   # Output of this prgram is inter data type. we need to explicitly define data type for interger or float data ype infornt of input function.
