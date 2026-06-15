38cc51bb1a0a1637b9ab52e8902eb98a980130e8
abhijitjagtap021-tech <abhijitjagtap021@gmail.com>
2026-06-15 20:48:46 +0530

    Add files via upload
    # Write a code which takes name , age  as input and also next year age 
    
    
    output - Hello Abhijit , you will turn 32 next year

---
+++ Assignment_Que_9.py
@@ -0,0 +1,11 @@
+# Write a code which takes name , age  as input and also next year age 
+
+print("Enter your name :")
+name = input()
+
+print("Enter your age :")   
+Age = int(input())        # 31
+
+Age = Age + 1      
+
+print("Hello" ,name,", you will turn" , Age ,"next year")   # output - Hello Abhijit , you will turn 32 next year
